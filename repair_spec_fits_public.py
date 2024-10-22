2882b6d29deebcc08b969de0822b0bb157572022
Kennicutt <fabricio.telescope@gmail.com>
2024-10-22 09:09:13 +0100

    Update repair_spec_fits_public.py

--- repair_spec_fits_public.py
+++ repair_spec_fits_public.py
@@ -1,17 +1,17 @@
-# This file is part of Public Astronomy.
+# This file is part of Public_Astronomy.
 #
-# Public Astronomy is free software: you can redistribute it and/or modify
+# Public_Astronomy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# any later version.
 #
-# Public Astronomy is distributed in the hope that it will be useful,
+# Public_Astronomy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Public Astronomy. If not, see <https://www.gnu.org/licenses/>.
+# along with Public_Astronomy. If not, see <https://www.gnu.org/licenses/>.
 
 
 import glob, sys
